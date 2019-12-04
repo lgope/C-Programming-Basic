f7cb94e0689a31757f8fe3c5f856880c20df5ed7
Lakshman Gope <lakshmangope012@gmail.com>
2019-12-04 21:41:06 +0600

    Initial commit 🗽🚀

---
+++ A+B Matrix.c
@@ -0,0 +1,82 @@
+#include<stdio.h>
+int main()
+{
+    int a[10][10], b[10][10], s[10][10];
+    int row, col, r, c;
+
+
+    printf("Enter the rows and cols.\n");
+    scanf("%d %d", &r, &c);
+
+
+    printf("Enter elements for A matrix:\n");
+    for(row = 0; row < r; row++)
+    {
+        for(col = 0; col < c; col++)
+        {
+            printf("a[%d][%d] = ", row,col);
+            scanf("%d", &a[row][col]);
+        }
+        printf("\n");
+    }
+
+
+    printf("Please enter elements for B matrix:\n");
+    for(row = 0; row < r; row++)
+    {
+        for(col = 0; col < c; col++)
+        {
+            printf("b[%d][%d] = ", row,col);
+            scanf("%d", &b[row][col]);
+        }
+        printf("\n\n");
+    }
+
+
+    printf("A = ");
+    for(row = 0; row < r; row++)
+    {
+        printf("\t");
+        for(col = 0; col < c; col++)
+        {
+            printf("%d ", a[row][col]);
+        }
+        printf("\n\n");
+    }
+
+
+    printf("B = ");
+    for(row = 0; row < r; row++)
+    {
+        printf("\t");
+        for(col = 0; col < c; col++)
+        {
+            printf("%d ", b[row][col]);
+        }
+        printf("\n\n");
+    }
+
+
+    for(row = 0; row < r; row++)
+    {
+        for(col = 0; col < c; col++)
+        {
+            s[row][col] = a[row][col] + b[row][col];
+        }
+        printf("\n");
+    }
+
+
+    printf("A+B = ");
+    for(row = 0; row < r; row++)
+    {
+        printf("\t");
+        for(col = 0; col < c; col++)
+        {
+            printf("%d ", s[row][col]);
+        }
+        printf("\n\n");
+    }
+
+    return 0;
+}
